542a1189a5fdbb2e5ae62a22546421f33a80eabf
cdz_ <smartcdz@163.com>
2018-04-24 09:24:08 +0800

    rabbitMQ集成

--- order/orderservice/src/main/java/com/cdz/orderservice/message/MyReceiver.java
+++ order/orderservice/src/main/java/com/cdz/orderservice/message/MyReceiver.java
@@ -2,7 +2,6 @@ package com.cdz.orderservice.message;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
